7d743d02065216a24a988cf28fe10f2b656ea342
Ruoyan Wong <kid.xiyang@gmail.com>
2012-04-15 01:34:16 -0400

    Change from oh-my-zsh

---
+++ zshrc/zsh/lib/aliases.zsh
@@ -0,0 +1,16 @@
+# Push and pop directories on directory stack
+alias pu='pushd'
+alias po='popd'
+
+# Basic directory operations
+alias ...='cd ../..'
+
+#alias g='grep -in'
+
+# Show history
+alias history='fc -l 1'
+
+# List direcory contents
+alias lsa='ls -lah'
+alias l='ls -la'
+alias ll='ls -l'
