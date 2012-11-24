58df284fb94edbeac33000efb3e4879ff03794a3
Ruoyan Wong <kid.xiyang@gmail.com>
2012-11-24 10:51:55 +0800

    Update bashrc/bash/alias.sh

--- bashrc/bash/alias.sh
+++ bashrc/bash/alias.sh
@@ -1,12 +1,11 @@
 
-
 function s {
     TMUX=`whereis tmux | awk '{ print $2 }'`
     $TMUX has-session -t default
     if [ $? = 1 ]; then
         $TMUX new-session -s default
     else
-    $TMUX attach-session -d -t default
+        $TMUX attach-session -d -t default
 fi 
 }
 
