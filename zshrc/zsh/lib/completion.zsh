b89fa88c3794cb6956c84c67d1febe57c2899888
root <root@li409-40.members.linode.com>
2012-12-01 13:28:00 +0000

    Add personal ssh directory and Update zsh completion config.

--- zshrc/zsh/lib/completion.zsh
+++ zshrc/zsh/lib/completion.zsh
@@ -1,14 +1,18 @@
-unsetopt menu_complete   # do not autoselect the first completion entry
-unsetopt flowcontrol
 setopt auto_menu         # show completion menu on succesive tab press
-#setopt complete_in_word
-#setopt always_to_end
+setopt complete_in_word
+setopt always_to_end
+# setopt glob bareglobqual nullglob rcexpandparam extendedglob unset
+
+unsetopt flowcontrol
+unsetopt menu_complete   # do not autoselect the first completion entry
+unsetopt markdirs globsubst shwordsplit shglob ksharrays cshnullglob
+unsetopt allexport aliases errexit octalzeroes
 
 WORDCHARS=''
 
 zmodload -i zsh/complist
 
-# use ~/.ssh/config hostname completion
+# Use ~/.ssh/config hostname completion
 [ -r ~/.ssh/config ] && _ssh_hosts=(`awk 'BEGIN {ORS=" "} /^Host/ { print $2 }' $HOME/.ssh/config`) || _ssh_hosts=()
 hosts=(
   "$_ssh_hosts[@]"
@@ -18,12 +22,10 @@ zstyle ':completion:*:hosts' hosts $hosts
 
 # Don't complete uninteresting users
 zstyle ':completion:*:*:*:users' ignored-patterns \
-        adm amanda apache avahi beaglidx bin cacti canna clamav daemon \
+        abrt adm amanda apache avahi beaglidx bin cacti canna clamav daemon \
         dbus distcache dovecot fax ftp games gdm gkrellmd gopher \
         hacluster haldaemon halt hsqldb ident junkbust ldap lp mail \
         mailman mailnull mldonkey mysql nagios \
         named netdump news nfsnobody nobody nscd ntp nut nx openvpn \
-        operator pcap postfix postgres privoxy pulse pvm quagga radvd \
-        rpc rpcuser rpm shutdown squid sshd sync uucp vcsa xfs
-
-zstyle ':completion:*:*:(ssh|scp):*:*' hosts `sed 's/^\([^ ,]*\).*$/\1/' ~/.ssh/known_hosts`
+        operator pcap postfix postgres privoxy pulse pvm quagga radvd root\
+        rpc rpcuser rpm saslauth shutdown squid sshd sync tcpdump uucp vcsa xfs
