f0c33c3f86f9442166962b36e7a4b1bf29bdadf4
saipanno <kid.xiyang@gmail.com>
2012-12-03 22:12:51 +0800

    匹配规则忽略大小写

--- zshrc/zsh/lib/completion.zsh
+++ zshrc/zsh/lib/completion.zsh
@@ -1,12 +1,30 @@
-unsetopt menu_complete # do not autoselect the first completion entry
-unsetopt flowcontrol
 setopt auto_menu # show completion menu on succesive tab press
-setopt complete_in_word
 setopt always_to_end
+setopt complete_in_word
+
+unsetopt flowcontrol
+unsetopt menu_complete # do not autoselect the first completion entry
 
 WORDCHARS=''
 
 zmodload -i zsh/complist
+
+## case-insensitive (all),partial-word and then substring completion
+if [ "x$CASE_SENSITIVE" = "xtrue" ]; then
+    zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+    unset CASE_SENSITIVE
+else
+    zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+fi
+
+zstyle ':completion:*' list-colors ''
+
+zstyle ':completion:*:*:*:*:*' menu select
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+
+# disable named-directories autocompletion
+zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
+cdpath=(.)
 
 # Use ~/.ssh/config hostname completion
 [ -r ~/.ssh/config ] && _ssh_hosts=(`awk '/^Host/ { print $2 }' $HOME/.ssh/config`) || _ssh_hosts=()
