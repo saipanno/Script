5153aecfc54d2d1e220daa9a279f952ec2ab4d4b
saipanno <kid.xiyang@gmail.com>
2012-12-10 22:42:10 +0800

    创建auto_login.py,作为auto_login.expect程序interact模式的前端

--- script/auto_login.py
+++ script/auto_login.py
@@ -10,20 +10,46 @@
 #    Created by Ruoyan Wong on 2012/11/06.
 
 import os
+import re
+import sys
 import subprocess
 from argparse import ArgumentParser
 
 if __name__ == '__main__':
 
-    HOME = os.e
+    TIMEOUT = 15
+    HOME = os.environ['HOME']
+    AUTO_EXPECT = '%s/bin/auto_login.expect' % HOME
+
+    opts = dict()
+    if re.search('ssh.ku', sys.argv[0]) is not None:
+        opts['port'] = 22
+        opts['username'] = 'root'
+        opts['secret'] = '%s/.ssh/id_rsa.ku' % HOME
+        opts['shadow'] = '%s/.ssh/password.ku' % HOME
+    elif re.search('ssh.kr', sys.argv[0]) is not None:
+        opts['port'] = 22
+        opts['username'] = 'sndawangruoyan'
+        opts['address'] = '122.11.32.36'
+        opts['secret'] = '%s/.ssh/id_rsa' % HOME
+        opts['shadow'] = '%s/.ssh/password' % HOME
+    elif re.search('ssh.hc', sys.argv[0]) is not None:
+        opts['port'] = 7035
+        opts['username'] = 'root'
+        opts['address'] = '106.187.89.40'
+        opts['secret'] = '%s/.ssh/id_rsa' % HOME
+        opts['shadow'] = '%s/.ssh/password' % HOME
 
     parser = ArgumentParser()
-    parser.add_argument('-a', dest='address',  help='server address', required=True)
-    parser.add_argument('-u', dest='username', help='username, (default: %(default)s)', default='root')
-    parser.add_argument('-p', dest='port',     help='port, (default: %(default)s)', default=22)
-    parser.add_argument('-i', dest='secret',   help='user identity file, (default: %(default)s)', default='%s/.ssh/id_rsa' % HOME)
-    parser.add_argument('-s', dest='shadow',   help='user password file, (default: %(default)s)', default='%s/.ssh/password' % HOME)
-    opts = vars(parser.parse_args())
+    parser.add_argument('-a', dest='address',  help='server address')
+    parser.add_argument('-u', dest='username', help='username')
+    parser.add_argument('-p', dest='port',     help='port')
+    parser.add_argument('-i', dest='secret',   help='user identity file')
+    parser.add_argument('-s', dest='shadow',   help='user password file')
 
-    command = 'expect %s u %s h %s p %s i %s s %s' % (opts['script'], opts['username'], opts['address'], opts['port'], opts['secret'], opts['shadow'])
-    subprocess.call
+    for key,value in vars(parser.parse_args()).items():
+        if value is not None:
+            opts[key] = value
+
+    command = 'expect %s o %s u %s a %s p %s i %s s %s t %s' % (AUTO_EXPECT, 'interact', opts['username'], opts['address'], opts['port'], opts['secret'], opts['shadow'], TIMEOUT)
+    subprocess.call('%s' % command, shell=True)
