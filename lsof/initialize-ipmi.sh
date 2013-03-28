454c1dcc32de57ed9e760e846540baaaa3662f42
saipanno <kid.xiyang@gmail.com>
2013-03-28 17:00:15 +0800

    统一IPMI配置脚本

--- lsof/initialize-ipmi.sh
+++ lsof/initialize-ipmi.sh
@@ -1,15 +1,27 @@
-MANUFACTURER=`dmidecode -t 1 | awk 'BEGIN {DEF="NONE"} /Manufacturer/{ DEF=$2 } END { print DEF }'`
-PRODUCT=`dmidecode -t 1 | awk 'BEGIN {DEF="NONE"} /Product Name/ { DEF=$4 } END { print DEF }'`
+yum -q -y install dmidecode ipmitool OpenIPMI
 
-yum -q -y install ipmitool OpenIPMI
+IPMI_USERNAME=ADMIN
+IPMI_PASSWORD=ADMIN
+
+IPMI_ADDRESS=`echo {ip} | awk 'BEGIN{FS="."} { print $3"."$4 }'`
+MANUFACTURER=`dmidecode -t 1 | awk 'BEGIN {RESULT="NONE"} /Manufacturer/{ RESULT=$2 } END { print RESULT }'`
+PRODUCT_NAME=`dmidecode -t 1 | awk 'BEGIN {RESULT="NONE"} /Product Name/{ RESULT=$4 } END { print RESULT }'`
+
+if [ "$PRODUCT_NAME" == "RH2285" ];then
+    IPMI_CHANNEL_ID=2
+    IPMI_USER_ID=3
+else
+    IPMI_CHANNEL_ID=1
+    IPMI_USER_ID=3
+fi
+
 service ipmi start
-IPMI=`echo {ip} | awk 'BEGIN{FS="."} { print $3"."$4 }'`
-ipmitool lan set 1 ipaddr 10.254.$IPMI
-ipmitool lan set 1 netmask 255.255.0.0 
-ipmitool lan set 1 defgw ipaddr 10.254.0.1
-ipmitool lan set 1 access on
-ipmitool user set name 3 saipanno 
-ipmitool user set password 3 saipanno.com
-ipmitool user enable 3
-ipmitool channel setaccess 1 3 callin=on ipmi=on link=on privilege=4
+ipmitool lan set $IPMI_CHANNEL_ID ipaddr 10.254.$IPMI_ADDRESS
+ipmitool lan set $IPMI_CHANNEL_ID netmask 255.255.0.0 
+ipmitool lan set $IPMI_CHANNEL_ID defgw ipaddr 10.254.0.1
+ipmitool lan set $IPMI_CHANNEL_ID access on
+ipmitool user set name $IPMI_USER_ID $IPMI_USERNAME
+ipmitool user set password $IPMI_USER_ID $IPMI_PASSWORD
+ipmitool user enable $IPMI_USER_ID
+ipmitool channel setaccess $IPMI_CHANNEL_ID $IPMI_USER_ID callin=on ipmi=on link=on privilege=4
 service ipmi stop
